ae4db0f682fcd9d9633162640df69cda574f4df9
Stephan Pauxberger <stephan@blackbuild.com>
2026-05-24 23:01:53 +0200

    Implementation for JSR380 validation

---
+++ klum-ast-bean-validation/src/test/groovy/com/blackbuild/klum/ast/validation/bean/JSR380ValidatorTest.groovy
@@ -0,0 +1,142 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2025 Stephan Pauxberger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.blackbuild.klum.ast.validation.bean
+
+import com.blackbuild.groovy.configdsl.transform.Validate
+import com.blackbuild.klum.ast.util.AbstractRuntimeTest
+import com.blackbuild.klum.ast.util.KlumValidationException
+import com.blackbuild.klum.ast.validation.SingleObjectValidationHandler
+import spock.lang.Issue
+
+@Issue("258")
+class JSR380ValidatorTest extends AbstractRuntimeTest {
+
+    void "empty validation works"() {
+        given:
+        createInstance('''
+            package pk
+
+            @DSL
+            class Foo extends TestObject {
+            }
+        ''')
+
+        when:
+        validate(instance)
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "simple validation with JBV 2.0 annotations"() {
+        given:
+        createInstance('''
+            package pk
+
+import jakarta.validation.constraints.Min
+
+            @DSL
+            class Foo extends TestObject {
+                // since we don't use AST-Transformation, we need to explicitly use assert
+                @Min(20L)
+                int value
+            }
+        ''')
+
+        when:
+        validate(instance)
+
+        then:
+        thrown(KlumValidationException)
+
+        when:
+        createInstance()
+        instance.value = 200
+        validate(instance)
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "simple validation with inheritance"() {
+        given:
+        createClass('''
+            package pk
+
+import jakarta.validation.constraints.Min
+
+            @DSL
+            class Foo extends TestObject {
+                // since we don't use AST-Transformation, we need to explicitly use assert
+                @Min(20L)
+                int value
+            }
+            
+            @DSL
+            class Bar extends Foo {
+                // since we don't use AST-Transformation, we need to explicitly use assert
+                @Min(20L)
+                int value2
+            }
+        ''')
+        createInstanceOf("pk.Bar")
+
+        when:
+        validate(instance)
+
+        then:
+        thrown(KlumValidationException)
+
+        when: "only parent value satisfied"
+        createInstanceOf("pk.Bar")
+        instance.value = 200
+        validate(instance)
+
+        then:
+        thrown(KlumValidationException)
+
+        when: "only child value satisfied"
+        createInstanceOf("pk.Bar")
+        instance.value2 = 200
+        validate(instance)
+
+        then:
+        thrown(KlumValidationException)
+
+        when: "both values satisfied"
+        createInstanceOf("pk.Bar")
+        instance.value = 200
+        instance.value2 = 200
+        validate(instance)
+
+        then:
+        noExceptionThrown()
+    }
+
+
+    private static void validate(Object instance) {
+        def validator = new SingleObjectValidationHandler(instance)
+        validator.execute().throwOn(Validate.Level.ERROR);
+    }
+}
