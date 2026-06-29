cab4e134213400247bfd6a9b45e3b77bd30e388d
Stephan Pauxberger <stephan@blackbuild.com>
2026-06-29 09:23:42 +0200

    adjust gradle plugin
    note that default for gradle plugin was already groovy 3

--- klum-ast-gradle-plugin/src/main/java/com/blackbuild/klum/ast/gradle/convention/GroovyVersion.java
+++ klum-ast-gradle-plugin/src/main/java/com/blackbuild/klum/ast/gradle/convention/GroovyVersion.java
@@ -25,9 +25,10 @@ package com.blackbuild.klum.ast.gradle.convention;
 
 public enum GroovyVersion {
 
-    GROOVY_24("org.codehaus.groovy:groovy-all:2.4.21", "org.spockframework:spock-core:1.3-groovy-2.4"),
-    GROOVY_3("org.codehaus.groovy:groovy-all:3.0.23", "org.spockframework:spock-core:2.3-groovy-3.0"),
-    GROOVY_4("org.apache.groovy:groovy-all:4.0.24", "org.spockframework:spock-core:2.3-groovy-4.0");
+    GROOVY_3("org.codehaus.groovy:groovy-all:3.0.23", "org.spockframework:spock-core:2.4-groovy-3.0"),
+    GROOVY_4("org.apache.groovy:groovy-all:4.0.24", "org.spockframework:spock-core:2.4-groovy-4.0"),
+    GROOVY_5("org.apache.groovy:groovy-all:5.0.0", "org.spockframework:spock-core:2.4-groovy-5.0");
+
 
     private final String spockDependency;
     private final String groovyDependency;
